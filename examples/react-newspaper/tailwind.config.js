2dc01b1d09139e378841d042d01777c12d80ae27
Jordi Burgos <jordiburgos@gmail.com>
2025-05-30 19:01:49 +0200

    Fix tailwindcss

--- examples/react-newspaper/tailwind.config.js
+++ examples/react-newspaper/tailwind.config.js
@@ -1,6 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme';
-
 export default {
   content: [
     "./index.html",
@@ -9,12 +7,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans], // Keep a good sans-serif default
-        serif: ['Georgia', 'Times New Roman', 'Times', ...defaultTheme.fontFamily.serif],
+        sans: ['Inter var', 'ui-sans-serif', 'system-ui', 'sans-serif'], // Keep a good sans-serif default
+        serif: ['Georgia', 'Times New Roman', 'Times', 'serif'],
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/typography'), // For prose styling if not already implicitly handled
-  ],
 }
